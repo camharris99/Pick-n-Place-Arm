dec601e26cceb2aafad75fce98d9aa40ec339bce
ctelwell <ctelwell@umich.edu>
2023-01-20 02:14:08 +0000

    Update control_station.py

--- control_station.py
+++ control_station.py
@@ -153,8 +153,8 @@ class Gui(QMainWindow):
         #self.ui.rdoutTheta.setText(str("%+.2f" % (pos[4])))
         #self.ui.rdoutPsi.setText(str("%+.2f" % (pos[5])))
 
-    @pyqtSlot(QImage, QImage, QImage)
-    def setImage(self, rgb_image, depth_image, tag_image):
+    @pyqtSlot(QImage, QImage, QImage, QImage)
+    def setImage(self, rgb_image, depth_image, tag_image, grid_image):
         """!
         @brief      Display the images from the camera.
 
@@ -167,6 +167,9 @@ class Gui(QMainWindow):
             self.ui.videoDisplay.setPixmap(QPixmap.fromImage(depth_image))
         if (self.ui.radioUsr1.isChecked()):
             self.ui.videoDisplay.setPixmap(QPixmap.fromImage(tag_image))
+        if (self.ui.radioUsr2.isChecked()):
+            self.ui.videoDisplay.setPixmap(QPixmap.fromImage(grid_image))
+
 
     """ Other callback functions attached to GUI elements"""
 

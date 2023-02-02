00dd93858768a0b96a988794edcc45ded7ba4fae
Cam Harris <harricam@umich.edu>
2023-02-02 11:50:42 -0500

    block detection contours works off console:

--- test/cam_block_detect_testing.py
+++ test/cam_block_detect_testing.py
@@ -72,34 +72,70 @@ def sliders(img):
 
     cv2.destroyAllWindows()
 
+def mask_and_contour(output,color):
+    if (color == "red"):
+        mask1 = cv2.inRange(output, (174,50,50), (182, 255, 255))
+        mask2 = cv2.inRange(output, (0,50,50), (3, 255, 255))
+        mask = mask1 + mask2
+        contour_color = (0,0,255)
+    elif (color == "orange"):
+        mask = cv2.inRange(output, (4,140,149), (12, 246, 218))
+        contour_color = (0,165,255)
+    elif (color == "yellow"):
+        mask = cv2.inRange(output, (20,135,160), (27, 255, 255))
+        contour_color = (0,250,250)
+    elif (color == "green"):
+        mask = cv2.inRange(output, (60,50,70), (90, 255, 255))
+        contour_color = (0,255,0)
+    elif (color == "blue"):
+        mask = cv2.inRange(output, (100,115,90), (107, 255, 255))
+        contour_color = (255,0,0)
+    elif (color == "purple"):
+        mask = cv2.inRange(output, (109,43,56), (135, 255, 255))
+        contour_color = (128,0,128)
 
-def main():
-
-    img = cv2.imread("test\my_all_blocks.png")
-
-    # sliders(imgitg)
-
-    # sharpen = np.array([[0,-1,0], [-1,5,-1], [0,-1,0]])
-    # img = cv2.filter2D(src=img, ddepth=-1, kernel=sharpen)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    mask_red = cv2.inRange(img_hsv, (174,50,50), (190, 255, 255))
-    mask_orange = cv2.inRange(img_hsv, (4,167,152), (14, 250, 255))
-    mask_yellow = cv2.inRange(img_hsv, (15,200,100), (35, 255, 255))
-    mask_green = cv2.inRange(img_hsv, (60,50,70), (90, 255, 255))
-    mask_blue = cv2.inRange(img_hsv, (100,115,90), (107, 255, 255))
-    mask_purple = cv2.inRange(img_hsv, (109,43,56), (135, 255, 255))
-    # mask_pink =
-
-    mask = mask_red + mask_yellow + mask_green + mask_orange + mask_blue + mask_purple
-    output = img.copy()
     output[np.where(mask==0)] = 0
-    # output = cv2.medianBlur(output,3)
+    output = cv2.medianBlur(output,3)
     output = cv2.morphologyEx(output, cv2.MORPH_OPEN, np.ones((7,7),np.uint8))
     output = cv2.morphologyEx(output, cv2.MORPH_CLOSE, np.ones((7,7),np.uint8))
 
+    _, output = cv2.threshold(output, 70,255, cv2.THRESH_BINARY)
+    output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    contours, _ = cv2.findContours(output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    output = cv2.cvtColor(output, cv2.COLOR_GRAY2BGR)
+
+    for i in range(len(contours)):
+        cv2.drawContours(output, contours, i, contour_color, 3)
+
+    moments = get_moments(output, contours)
+    return output
+
+def get_moments(output, contours):
+    pass
+
+def color_seg(img):
+
+    img = cv2.imread("test\my_all_blocks.png")
+
+    # sliders(img)
+
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    output = mask_and_contour(img_hsv.copy(), "red")
+    
     cv2.imshow("image", output)
     cv2.waitKey(0)
+
+
+
+def main():
+
+    img_color = cv2.imread("test\my_all_blocks.png")
+    img_depth = cv2.imdecode
+
+    # sliders(img_color)
+
+    color_seg(img_color)
 
 if __name__ == "__main__":
     main()
